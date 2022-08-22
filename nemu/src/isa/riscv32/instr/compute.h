343622a54be22a2bd18665b71643185a7da2339a
tracer-ics2021 <tracer@njuics.org>
2022-08-22 22:27:41 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: e987531bcef4499da9013546e2a8be75     Virtualization: microsoft   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  22:27:41 up  1:14,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -1,3 +1,12 @@
 def_EHelper(lui) {
   rtl_li(s, ddest, id_src1->imm);
 }
+
+def_EHelper(auipc) {
+  printf("PC=%x\n", s->pc);
+  rtl_addi(s, ddest, &(s->pc), id_src1->imm);
+}
+
+def_EHelper(addi) {
+  rtl_addi(s, ddest, dsrc1, id_src2->imm);
+}
--- nemu/src/isa/riscv32/instr/ldst.h
+++ nemu/src/isa/riscv32/instr/ldst.h
@@ -5,3 +5,13 @@ def_EHelper(lw) {
 def_EHelper(sw) {
   rtl_sm(s, ddest, dsrc1, id_src2->imm, 4);
 }
+
+def_EHelper(jal) {
+  rtl_addi(s, &(s->dnpc), &(s->pc), id_src1->imm);
+  rtl_mv(s, ddest, &(s->snpc));
+}
+
+def_EHelper(jalr) {
+  rtl_addi(s, &(s->dnpc), dsrc1, id_src2->imm);
+  rtl_mv(s, ddest, &(s->snpc));
+}
