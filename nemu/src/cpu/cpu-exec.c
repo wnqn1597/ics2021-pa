eae23a095fa82cd8f8745900eadad9b20933a92f
tracer-ics2021 <tracer@njuics.org>
2022-08-23 22:30:54 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 97b794d941c44f828a3e82d0cba7219c     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  22:30:54 up 58 min,  1 user,  load average: 0.41, 0.11, 0.04

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -31,6 +31,8 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
+  printf("%x: %x\n", _this->pc, _this->isa.instr.val);
+
   //TODO: watchingpoint
   WP *now = get_head();
   while(now != NULL){
