1dfbb0530f90b511ebdb881b0c877cec5e5b8888
tracer-ics2021 <tracer@njuics.org>
2022-08-24 23:00:39 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 7679571cbc014934a55858389f0f1aef     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  23:00:39 up  4:27,  1 user,  load average: 0.03, 0.01, 0.00

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -88,6 +88,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   CPU_state ref_r;
 
   if (skip_dut_nr_instr > 0) {
+    printf("skip dut\n");
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
     if (ref_r.pc == npc) {
       skip_dut_nr_instr = 0;
@@ -101,12 +102,13 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   }
 
   if (is_skip_ref) {
+    printf("skip ref\n");
     // to skip the checking of an instruction, just copy the reg state to reference design
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
     is_skip_ref = false;
     return;
   }
-
+  printf("to here\n");
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
   
--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -3,11 +3,19 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  bool ret = true;
   for(int i = 0; i < 32; i++) {
-    if(ref_r->gpr[i]._32 != cpu.gpr[i]._32) return false;
+    if(ref_r->gpr[i]._32 != cpu.gpr[i]._32) ret = false;
   }
-  if(ref_r->pc != pc + 4) return false;
-  return true;
+  if(ref_r->pc != pc + 4) ret = false;
+  if(!ret){
+    printf("ID\tREF\t\tDUT\n");
+    for(int i = 0; i < 32; i++){
+      printf("%d\t%08x\t%08x\n", i, ref_r->gpr[i]._32, cpu.gpr[i]._32);
+    }
+    printf("refpc:%08x,dutpc:%08x\n", ref_r->pc, pc+4);
+  }
+  return ret;
 }
 
 void isa_difftest_attach() {
