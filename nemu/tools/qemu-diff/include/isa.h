7cb89e05dd40d8dcabdfca50fea11173bd5b4a30
tracer-ics2021 <tracer@njuics.org>
2022-08-24 21:08:33 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 7679571cbc014934a55858389f0f1aef     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  21:08:33 up  2:34,  1 user,  load average: 0.14, 0.07, 0.02

--- nemu/tools/qemu-diff/include/isa.h
+++ nemu/tools/qemu-diff/include/isa.h
@@ -18,27 +18,27 @@
 #error Unsupport ISA
 #endif
 
-union isa_gdb_regs {
-  struct {
-#if defined(CONFIG_ISA_mips32)
-    //uint32_t gpr[32];
-    uint32_t status, lo, hi, badvaddr, cause, pc;
-#elif defined(CONFIG_ISA_riscv32)
-    //uint32_t gpr[32];
-    uint32_t pc;
-#elif defined(CONFIG_ISA_riscv64)
-    //uint64_t gpr[32];
-    //uint64_t fpr[32];
-    uint64_t pc;
-#elif defined(CONFIG_ISA_x86)
-    //uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-    //uint32_t eip, eflags;
-    //uint32_t cs, ss, ds, es, fs, gs;
-#endif
-  };
-  struct {
-    uint32_t array[77];
-  };
-};
+//union isa_gdb_regs {
+//  struct {
+//#if defined(CONFIG_ISA_mips32)
+//    uint32_t gpr[32];
+//    uint32_t status, lo, hi, badvaddr, cause, pc;
+//#elif defined(CONFIG_ISA_riscv32)
+//    uint32_t gpr[32];
+//    uint32_t pc;
+//#elif defined(CONFIG_ISA_riscv64)
+//    uint64_t gpr[32];
+//    uint64_t fpr[32];
+//    uint64_t pc;
+//#elif defined(CONFIG_ISA_x86)
+//    uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+//    uint32_t eip, eflags;
+//    uint32_t cs, ss, ds, es, fs, gs;
+//#endif
+//  };
+//  struct {
+//    uint32_t array[77];
+//  };
+//};
 
 #endif
