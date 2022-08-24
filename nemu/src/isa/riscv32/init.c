8932bfcb2b0c8ea8ef01082fa409b85abcfa2a09
tracer-ics2021 <tracer@njuics.org>
2022-08-24 19:46:58 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 7679571cbc014934a55858389f0f1aef     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  19:46:58 up  1:13,  1 user,  load average: 0.16, 0.03, 0.01

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -4,12 +4,12 @@
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
 
-static const uint32_t img [] = {
-  0x800002b7,  // lui t0,0x80000
-  0x0002a023,  // sw  zero,0(t0)
-  0x0002a503,  // lw  a0,0(t0)
-  0x0000006b,  // nemu_trap
-};
+//static const uint32_t img [] = {
+//  0x800002b7,  // lui t0,0x80000
+//  0x0002a023,  // sw  zero,0(t0)
+//  0x0002a503,  // lw  a0,0(t0)
+//  0x0000006b,  // nemu_trap
+//};
 
 //static const uint32_t img [] = {
 //  0x80006f,
@@ -21,6 +21,41 @@ static const uint32_t img [] = {
 //  0x110193,
 //  0x0000006b,
 //};
+
+static const uint32_t img [] = {
+  0x00001037,
+  0x000010b7,
+  0x00001137,
+  0x000011b7,
+  0x00001237,
+  0x000012b7,
+  0x00001337,
+  0x000013b7,
+  0x00001437,
+  0x000014b7,
+  0x00001537,
+  0x000015b7,
+  0x00001637,
+  0x000016b7,
+  0x00001737,
+  0x000017b7,
+  0x00001837,
+  0x000018b7,
+  0x00001937,
+  0x000019b7,
+  0x00001a37,
+  0x00001ab7,
+  0x00001b37,
+  0x00001bb7,
+  0x00001c37,
+  0x00001cb7,
+  0x00001d37,
+  0x00001db7,
+  0x00001e37,
+  0x00001eb7,
+  0x00001f37,
+  0x00001fb7,
+};
 
 static void restart() {
   /* Set the initial program counter. */
