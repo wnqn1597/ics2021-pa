841f85e9a91e1ecc079f2f65ba910843032565b1
tracer-ics2021 <tracer@njuics.org>
2022-08-24 08:16:28 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 466e83bd184347408aefa76fcccc851a     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  08:16:28 up 21 min,  1 user,  load average: 0.17, 0.12, 0.05

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -9,12 +9,52 @@ int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+int vsprintf(char *buf, const char *fmt, va_list args) {
+    char *str;
+
+    for (str = buf; *fmt != '\0'; fmt++) {
+        if (*fmt != '%') {
+            *str++ = *fmt;
+            continue;
+        }
+        fmt++;
+        if(*fmt == 'd') {
+			int arg = va_arg(args, int);
+			int b = 1, c = 0;
+			while(arg / b != 0) {
+			  b *= 10;
+			  c++;
+			}
+			int a = c - 1;
+			while(arg > 0) {
+			  int r = arg % 10;
+			  *(str+a) = (char)('0' + r);
+			  arg /= 10;
+			  a--;
+			}
+			str += c;
+			break;
+        }else if(*fmt == 's') {
+            char *arg = va_arg(args, char*);
+            strcpy(str, arg);
+            str += strlen(arg);
+            break;
+        }
+    } /* end of for (str = buf; *fmt; fmt++) */
+
+    *str = '\0';
+    return str - buf;
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+int sprintf(char *buf, const char *fmt, ...) {
+    va_list args;
+    int n;
+
+    va_start(args, fmt);
+    n = vsprintf(buf, fmt, args);
+    va_end(args);
+
+    return n;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
