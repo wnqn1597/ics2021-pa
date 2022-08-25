563b1950afe285d6fb0c5228f0b9a79b009f2923
tracer-ics2021 <tracer@njuics.org>
2022-08-25 13:11:53 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 46cc5d6362a94fd49bae33f553abff71     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  13:11:53 up  2:15,  1 user,  load average: 0.01, 0.08, 0.05

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -16,7 +16,7 @@ void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->second = 0;
   rtc->minute = 0;
   rtc->hour   = 0;
-  rtc->day    = 1;
-  rtc->month  = 1;
+  rtc->day    = 0;
+  rtc->month  = 0;
   rtc->year   = 1900;
 }
