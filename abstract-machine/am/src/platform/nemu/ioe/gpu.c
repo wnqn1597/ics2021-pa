de2bfe193ca9ce905a824bb83ad2b45b41b5f7b7
tracer-ics2021 <tracer@njuics.org>
2022-08-25 23:37:00 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: e2b8246b68c04a4985d631348485e471     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  23:37:00 up  3:40,  1 user,  load average: 0.03, 0.03, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,6 +4,12 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  int w = 32;
+  int h = 32;
+  uint32_t *fb = (uint32_t*)(uintptr_t)FB_ADDR;
+  for(i = 0; i < w*h; i++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -20,6 +26,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
+
   }
 }
 
