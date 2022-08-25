41b985839d4ade0975e10199bbee7392ea481e1c
tracer-ics2021 <tracer@njuics.org>
2022-08-25 23:53:42 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: e2b8246b68c04a4985d631348485e471     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  23:53:42 up  3:57,  1 user,  load average: 0.00, 0.02, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -25,8 +25,14 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
+    uint32_t *fb = (uint32_t*)(uintptr_t)FB_ADDR;
     outl(SYNC_ADDR, 1);
-    
+    uint32_t bias = ctl->y * 400 + ctl->x;
+    for(int i = 0; i < ctl->h; i++) {
+      for(int j = 0; j < ctl->w; j++) {
+        fb[bias + i * 400 + j] = ((uint32_t*)(ctl->pixels))[i * ctl->w + j];
+      }
+    }
   }
 }
 
