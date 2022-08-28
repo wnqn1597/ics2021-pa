7e81ae5847a31e2b5f9da53073ca42524f687da0
tracer-ics2021 <tracer@njuics.org>
2022-08-28 22:57:10 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 4e28e1e3a6f244ccaafe9858d2a0eaf5     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  22:57:10 up  1:03,  1 user,  load average: 0.23, 0.12, 0.04

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,7 +13,7 @@ void sys_yield(Context *c) {
 }
 
 void sys_write(Context *c, int fd, void *buf, size_t count) {
-  printf("CALL WRITE\n");
+  //printf("CALL WRITE\n");
   if(fd == 1 || fd == 2) {
     for(int i = 0; i < count; i++) putch(*((char*)buf + i));
     c->GPRx = count;
@@ -23,6 +23,7 @@ void sys_write(Context *c, int fd, void *buf, size_t count) {
 }
 
 void sys_brk(Context *c, intptr_t addr) {
+  printf("CALL BRK\n");
   c->GPRx = 0;
 }
 
