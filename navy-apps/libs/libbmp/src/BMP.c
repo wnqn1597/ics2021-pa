2ebf0e9ab064f3f8e7e7a0c14cdbbf88bbebd2a5
tracer-ics2021 <tracer@njuics.org>
2022-08-30 22:05:51 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 408140c4aead434a968d37c8f05ca881     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  22:05:51 up  5:55,  1 user,  load average: 0.06, 0.09, 0.07

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -28,7 +28,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
 
   //if (hdr.bitcount != 24){printf("FUCKIN\n"); return NULL;}
-  if (hdr.compression != 0){printf("FUCKYOU\n"); return NULL;}
+  //if (hdr.compression != 0){printf("FUCKYOU\n"); return NULL;}
   int w = hdr.width;
   int h = hdr.height;
   uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
