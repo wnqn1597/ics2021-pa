601efd447d0295a947448dc0611d0aecbbdf13e5
tracer-ics2021 <tracer@njuics.org>
2022-08-30 12:54:01 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 399f42d8705f4990b60e8c20e3bb4ab6     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  12:54:01 up  3:48,  1 user,  load average: 0.11, 0.07, 0.02

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -116,11 +116,11 @@ void init_fs() {
   // TODO: initialize the size of /dev/fb
   char buf[20];
   (file_table[6].read)(buf, 0, 0);
-  printf("%s\n", buf);
   int i;
   for(i = 0; buf[i] != '\n'; i++);
   char width[10], height[10];
   strncpy(width, buf, i); strncpy(height, (char*)buf+i+1, 10);
+  printf("%s--%s\n", width, height);
   uint32_t res = atoi(width) * atoi(height) * 4;
   printf("SIZE OF FB: %d-%d\n", file_table[FD_FB].size, res);
 
