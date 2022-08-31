83ba3795bd1a8df962fc84200e59e4c7266e07c2
tracer-ics2021 <tracer@njuics.org>
2022-08-31 21:35:23 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: f665f06b24cd49bc815f3d066d621154     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  21:35:23 up  2:31,  1 user,  load average: 0.18, 0.15, 0.12

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -54,7 +54,7 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
 }
 
 void _exit(int status) {
-  int ret = _syscall_(SYS_exit, status, 0, 0);
+  int ret = 1;//_syscall_(SYS_exit, status, 0, 0);
   printf("here\n");
   if(ret == 1){
     printf("CHANGE PROC.\n");
