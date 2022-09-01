82d7cb0d6bd4aa6cc223414b426f677c3b16c688
tracer-ics2021 <tracer@njuics.org>
2022-09-01 23:05:11 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 4c62e74d756942e59b432f7b0feb0414     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  23:05:11 up 57 min,  1 user,  load average: 0.29, 0.16, 0.10

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -108,6 +108,9 @@ int main(int argc, char *argv[], char *envp[]) {
       exec_argv[2] = NULL;
       clear_display();
       SDL_UpdateRect(screen, 0, 0, 400, 300);
+      //close all file!!!
+      free(font);
+      printf("free font\n");
       execve(exec_argv[0], (char**)exec_argv, (char**)envp);
       fprintf(stderr, "\033[31m[ERROR]\033[0m Exec %s failed.\n\n", exec_argv[0]);
     } else {
