958512b0ead9626fc4c03ede43ce74a362c6f788
tracer-ics2021 <tracer@njuics.org>
2022-09-01 11:01:24 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 80dbe271a44149ca866a3b49b68feba9     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  11:01:24 up 20 min,  1 user,  load average: 0.17, 0.12, 0.08

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,13 +16,14 @@ int fs_open(const char *pathname, int flags, int mode);
 void* get_finfo(int index, int property);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  //TODO();
+  printf("OPEN FILE: %s ", filename);  
+
   int fd = fs_open(filename, 0, 0);
   if(fd == -1 && filename != NULL){
     printf("%s doesn't exist!!!\n", filename); 
     assert(0);
   }
-  
+  printf("fd = %d\n", fd);
   size_t offset = *((size_t*)get_finfo(fd, 2));
 
   Elf_Ehdr ehdr;
