bae0078b05516fd672f5c83364291c922365c5e2
tracer-ics2021 <tracer@njuics.org>
2022-09-01 21:29:29 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 80dbe271a44149ca866a3b49b68feba9     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  21:29:29 up 10:48,  1 user,  load average: 0.16, 0.06, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -70,7 +70,7 @@ int sprintf(char *buf, const char *fmt, ...) {
 
 int printf(const char *fmt, ...) {
   //char *buf;
-  char buf[200];
+  char buf[300];
   va_list args;
   int n;
   va_start(args, fmt);
