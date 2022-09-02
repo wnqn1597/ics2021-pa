34df3f591b719c4e1dbeb243cc6dd410f37ef451
tracer-ics2021 <tracer@njuics.org>
2022-09-02 15:38:28 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: c335fd98ee6f4c66a46679a19c74e610     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  15:38:28 up  2:47,  1 user,  load average: 0.55, 0.34, 0.23

---
+++ navy-apps/tests/test1/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_bdf.h>
+
+static BDF_Font *font = NULL;
+
+int main() {
+  font = new BDF_Font("/share/fonts/Courier-7.bdf");
+
+  printf("PASS TEST 1 with %d-%d\n", font->w, font->h);
+
+  return 0;
+}
