b90c1c82645c07a8a0019e911302a7e1132d4f50
tracer-ics2021 <tracer@njuics.org>
2022-09-04 16:28:37 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 7f9dcee7fc06475b98b3b59652f475d4     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  16:28:37 up 22 min,  1 user,  load average: 0.00, 0.03, 0.06

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -4,13 +4,37 @@
 #include <stdlib.h>
 #include <assert.h>
 
+void load_argv_envp(char *arg_buf[], char *env_buf[], void *start){
+    uint32_t* ustart = (uint32_t*)start;
+    ustart++;
+    for(int i = 0; i < argc; i++){
+        char *ptr = (char*)*ustart;
+        arg_buf[i] = ptr;
+        ustart++;
+    }
+    ustart++;
+    for(int i = 0; *ustart; i++){
+        char *ptr = (char*)*ustart;
+        env_buf[i] = ptr;
+        ustart++;
+    }
+}
+
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
   printf("call_main\n");
   printf("args:%x\n", *args);
-  char *empty[] =  {NULL };
-  environ = empty;
+
+  char *args[10] = {};
+  char *envs[10] = {};
+  load_argv_envp(args, envs, (void*)args);
+
+  printf("args:\n");
+  for(int i = 0; args[i] != NULL; i++) printf("%s\n", args[i]);
+  printf("envs:\n");
+  for(int i = 0; envs[i] != NULL; i++) printf("%s\n", envs[i]);
+
   exit(main(0, empty, empty));
   assert(0);
 }
