261dbee9c1b87ec058fce51bdc84fe1ee90ccac0
tracer-ics2021 <tracer@njuics.org>
2022-09-04 19:48:58 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 7f9dcee7fc06475b98b3b59652f475d4     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  19:48:58 up  3:43,  1 user,  load average: 0.12, 0.06, 0.01

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -123,7 +123,7 @@ Context* schedule(Context *prev) {
   //display_context(prev);
   current->cp = prev;
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-  //printf("from %x to %x\n", (uint32_t)prev, (uint32_t)current->cp);
+  printf("from %x to %x\n", (uint32_t)prev, (uint32_t)current->cp);
   return current->cp;
   //return NULL;
 }
