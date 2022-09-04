ed4c5637cd1c12f94d64cba97d23a39c37b3ac7d
tracer-ics2021 <tracer@njuics.org>
2022-09-04 10:54:12 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 5168aaaba1814927bbb70220ea3cc222     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  10:54:12 up 44 min,  1 user,  load average: 0.22, 0.26, 0.18

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -12,7 +12,6 @@ static Context* do_event(Event e, Context* c) {
 	    return c;
     case EVENT_SYSCALL: 
 	    //printf("SYSCALL START\n");
-	    //c->mepc += 4;
 	    break;
     default: panic("Unhandled event ID = %d", e.event);
   }
