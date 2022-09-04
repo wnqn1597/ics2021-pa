d2cae78196006b65c89fffce9b9b6cac2847f396
tracer-ics2021 <tracer@njuics.org>
2022-09-04 10:57:57 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 5168aaaba1814927bbb70220ea3cc222     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  10:57:57 up 47 min,  1 user,  load average: 0.09, 0.16, 0.15

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -30,14 +30,14 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    */
   csr_reg.mcause = NO;
   csr_reg.mepc = epc;
-  printf("ECALL PC=%x\n", epc);
+  //printf("ECALL PC=%x\n", epc);
   return csr_reg.mtvec;
 }
 
 word_t isa_out_intr(word_t NO) {
   csr_reg.mcause = NO;
   // not + 4
-  printf("MRET  PC=%x\n", csr_reg.mepc);
+  //printf("MRET  PC=%x\n", csr_reg.mepc);
   return csr_reg.mepc;
 }
 
