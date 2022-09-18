452b7b35f55fb6e54f22c9fa1615b6f4c4e2220a
tracer-ics2021 <tracer@njuics.org>
2022-09-18 20:18:35 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 527becb16c684f30aa2ac6d7b527eef4     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  20:18:35 up  4:58,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -2,6 +2,12 @@
 #include <memory/paddr.h>
 #include <memory/vaddr.h>
 
+int isa_mmu_check(vaddr_t vaddr, int len, int type) {
+  uint32_t satp_val = *(get_csr(0x180));
+  if((satp_val >> 31) == 1) return MMU_TRANSLATE;
+  else return MMU_DIRECT;
+}
+
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  return MEM_RET_FAIL;
+  return 0;  
 }
