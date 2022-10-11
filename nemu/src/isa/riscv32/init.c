dbd986e34c56e110ca382a1995696a8f4d3eac8e
tracer-ics2021 <tracer@njuics.org>
2022-10-11 17:40:52 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: d36cbaf4fbed4f26803f3b1bdfd51593     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  17:40:52 up  3:38,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -4,30 +4,13 @@
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
 
-//static const uint32_t img [] = {
-//  0x800002b7,  // lui t0,0x80000
-//  0x0002a023,  // sw  zero,0(t0)
-//  0x0002a503,  // lw  a0,0(t0)
-//  0x0000006b,  // nemu_trap
-//};
-
-//static const uint32_t img [] = {
-//  0x80006f,
-//  0x100006f,
-//  0x800000b7,
-//  0x408067,
-//  0x0,
-//  0x117,
-//  0x110193,
-//  0x0000006b,
-//};
-
 static const uint32_t img [] = {
-  0x00000413,
-  0x00009117,
-  0xffc10113,
-  0x0a8000ef,
+  0x800002b7,  // lui t0,0x80000
+  0x0002a023,  // sw  zero,0(t0)
+  0x0002a503,  // lw  a0,0(t0)
+  0x0000006b,  // nemu_trap
 };
+
 
 static void restart() {
   /* Set the initial program counter. */
