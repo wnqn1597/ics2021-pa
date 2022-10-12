91864b653ebac59094fc2d3379ecdf8539f137fa
tracer-ics2021 <tracer@njuics.org>
2022-10-12 23:14:57 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 8708e9f60f1243b1a65b4aa8a0308e7d     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  23:14:57 up  2:08,  1 user,  load average: 0.29, 0.18, 0.07

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-//#include <stdio.h>
+#include <stdio.h>
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
@@ -73,6 +73,8 @@ SDL_Surface* SDL_CreateRGBSurface(uint32_t flags, int width, int height, int dep
   SDL_Surface *s = malloc(sizeof(SDL_Surface));
   assert(s);
 	printf("flags=%d\n", flags);
+	assert(0);
+
   s->flags = flags;
   s->format = malloc(sizeof(SDL_PixelFormat));
   assert(s->format);
