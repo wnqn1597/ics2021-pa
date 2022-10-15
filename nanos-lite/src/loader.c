0b1b71ce1b59cca7854b744c53476e1e8f5f86fa
tracer-ics2021 <tracer@njuics.org>
2022-10-15 23:59:30 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 7d4eb59a07964efb8c533f0c92f9bef1     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  23:59:30 up  8:00,  1 user,  load average: 0.02, 0.06, 0.01

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -37,9 +37,10 @@ uintptr_t loader(PCB *pcb, const char *filename) {
 			size_t rptr = offset + phdr[i].p_offset;
 			int size = phdr[i].p_memsz + phdr[i].p_vaddr - (phdr[i].p_vaddr & ~0xfff);
 			char *vptr = (char*)(phdr[i].p_vaddr & ~0xfff);
+			// FIXME: more precise read ramdisk
 			while(size > 0){
 				void *pptr = new_page(1);
-				printf("vptr=%p, pptr=%p\n", vptr, pptr);
+				//printf("vptr=%p, pptr=%p\n", vptr, pptr);
 				map(&(pcb->as), (void*)vptr, pptr, 0);
 				ramdisk_read((void*)vptr, rptr, PGSIZE);
 				rptr += PGSIZE;
