b65332c54bde2d5558acec19b5e59d6e231445c8
tracer-ics2021 <tracer@njuics.org>
2022-10-16 20:17:59 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 7882df3e0c484302be7e36a23b715315     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  20:17:59 up 11:04,  1 user,  load average: 0.15, 0.03, 0.01

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -75,7 +75,7 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-				printf("\n");
+				putch('a');
   intptr_t ret = _syscall_(SYS_brk, brk + increment, 0, 0);
   //if(ret == 0) {
   //  intptr_t pre_brk = brk;
