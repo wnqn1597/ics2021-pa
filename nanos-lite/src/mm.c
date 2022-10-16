b41380ca1627b086234deceafa2b8d16a0b155f9
tracer-ics2021 <tracer@njuics.org>
2022-10-16 17:47:58 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 7882df3e0c484302be7e36a23b715315     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  17:47:58 up  8:34,  1 user,  load average: 0.03, 0.01, 0.00

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -7,12 +7,11 @@ extern PCB *current;
 void* new_page(size_t nr_page) {
   void *ret = pf;
   pf += nr_page * PGSIZE;
-	printf("pf=%p\n", pf);
-  if((uintptr_t)pf > (uintptr_t)heap.end) {
-    printf("Insufficient memory.\n");
-    pf = ret;
-    return NULL;
-  }
+  //if((uintptr_t)pf > (uintptr_t)heap.end) {
+  //  printf("Insufficient memory.\n");
+  //  pf = ret;
+  //  return NULL;
+  //}
   return ret;
 }
 
