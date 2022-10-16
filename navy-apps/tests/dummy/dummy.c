56cdf466adeb6a7d479949ec5eb8d1508d61de1d
tracer-ics2021 <tracer@njuics.org>
2022-10-16 10:53:57 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 7882df3e0c484302be7e36a23b715315     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  10:53:58 up  1:40,  1 user,  load average: 0.11, 0.12, 0.06

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -9,8 +9,5 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-	printf("dummy start\n");
-  int ret = _syscall_(SYS_yield, 0, 0, 0);
-	printf("dummy ret %d\n", ret);
-	return ret;
+  return _syscall_(SYS_yield, 0, 0, 0);
 }
