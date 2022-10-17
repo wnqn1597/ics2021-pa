943894ab1c66bda8db866256b715abf2fda3cb1b
tracer-ics2021 <tracer@njuics.org>
2022-10-17 13:32:09 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 929f7b5b1946457a9837d1c0bf1de851     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  13:32:09 up 48 min,  1 user,  load average: 0.04, 0.03, 0.00

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,7 +8,7 @@ void __am_get_cur_as(Context *c);
 void __am_switch(Context *c);
 
 Context* __am_irq_handle(Context *c) {
-	//printf("Irq accept context %p, mcause=%d\n", c, c->mcause);
+	printf("Irq accept context %p, mcause=%08x\n", c, c->mcause);
 	// Page
 	__am_get_cur_as(c);
 
