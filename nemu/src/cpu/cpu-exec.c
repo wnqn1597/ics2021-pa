2a87b695dec58fd2998db5106c0408ce30df7b1b
tracer-ics2021 <tracer@njuics.org>
2022-10-17 14:35:20 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 929f7b5b1946457a9837d1c0bf1de851     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  14:35:20 up  1:51,  1 user,  load average: 0.16, 0.16, 0.12

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -149,7 +149,7 @@ void cpu_exec(uint64_t n) {
 		// device intr
 		word_t intr = isa_query_intr();
 		if(intr != INTR_EMPTY){
-			cpu.pc = isa_raise_intr(intr, cpu.pc);
+			cpu.pc = isa_raise_intr(intr, cpu.pc-4);
 		}
 
   }
