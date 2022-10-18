3413e4c53da1b8f58398b64b2e9f5dded9ad9bdc
tracer-ics2021 <tracer@njuics.org>
2022-10-18 15:56:13 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 37b456d27d71448d9526c339c330dcd3     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  15:56:13 up  1:29,  1 user,  load average: 0.03, 0.03, 0.00

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -58,7 +58,7 @@ extern void __am_asm_trap(void);
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
-	printf("_end=%p\n", &_end);
+	asm volatile("csrw mscratch, %0" : : "r"(&_end));
   // register event handler
   user_handler = handler;
 
