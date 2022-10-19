835408f791adcd48d41500e134d942368ef9a12b
tracer-ics2021 <tracer@njuics.org>
2022-10-19 11:36:02 +0800

    >  compile 211300005    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3146be6e57a46ceb877cd2b0511bf4c            Boot ID: 652e34cfa8cc4e34b2bea50321a262f3     Virtualization: microsoft   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  11:36:02 up 44 min,  1 user,  load average: 0.02, 0.12, 0.10

--- nemu/src/isa/riscv32/instr/special.h
+++ nemu/src/isa/riscv32/instr/special.h
@@ -9,8 +9,8 @@ def_EHelper(nemu_trap) {
 }
 
 def_EHelper(ecall) {
-  //s->dnpc = isa_raise_intr(((int)cpu.gpr[17] < 0) ? 0x80000003 : 0xb , s->pc);
-  s->dnpc = isa_raise_intr((int)cpu.gpr[17]._32, s->pc);
+  s->dnpc = isa_raise_intr(((int)cpu.gpr[17]._32 < 0) ? 0x80000003 : 0xb , s->pc);
+  //s->dnpc = isa_raise_intr((int)cpu.gpr[17]._32, s->pc);
 }
 
 def_EHelper(mret) {
